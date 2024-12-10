82804ec50fde67b846e096e97221df14a09e5a8c
FrancoA56 <franco.adamoli@gmail.com>
2024-12-10 17:35:16 -0300

    estructura base

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+      extend: {
+        /*
+        colors: {
+        },
+        
+        fontSize: {
+          parrafo: "1.2rem",
+          subtitulo: "1.5rem",
+          semititulo: "2.2rem",
+          titulo: "3rem",
+        },
+        fontFamily: {
+          gothamB: 'Gotham-Book',
+          gothamBI: 'Gotham-BookItalic',
+          BodoniB: 'BodoniStd-Book',
+          BodoniBI: 'BodoniStd-BookItalic',
+        }*/
+      }
+    },
+    plugins: [],
+  };
+  
